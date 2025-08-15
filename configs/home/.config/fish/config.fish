d576799f8b57a7c38f9186ac250732b2e6a96546
Nicholas Gerard <86604749+sirdich@users.noreply.github.com>
2025-08-15 09:45:49 +0900

    .

--- configs/home/.config/fish/config.fish
+++ configs/home/.config/fish/config.fish
@@ -1,3 +1,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+fastfetch
+alias yayf="yayfzf"
+alias vim="nvim"
+alias svim="sudo nvim"
+alias ..="cd .."
+alias cp="cp -i"
+alias mv="mv -i"
+alias ll="ls -la"
+alias rm="rm -rf"
+alias bios="systemctl reboot --firmware-setup"
